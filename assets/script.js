6a16ad3976105492881bc84526ee826c1fe29415
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-04-27 14:40:24 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,81 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const items = document.querySelectorAll('.slider-img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0; 
+let afficheText = document.querySelector(".affiche_text");
+afficheText.innerHTML = `${slides[count].tagLine}`;
+
+const puces = document.querySelectorAll('.puce');
+const pucesActive = document.querySelector('.activee')
+const nbPuces = puces.length;
+
+function slideSuivante(){
+	items[count].classList.remove('active');
+	puces[count].classList.remove('activee');
+
+	if(count < nbSlide - 1){
+		count++;
+	}else {
+		count = 0;
+	}
+
+	items[count].classList.add('active');
+	puces[count].classList.add('activee');
+
+	afficheText.innerHTML = slides[count].tagLine;
+	
+
+	console.log(count);
+	console.log(slides[count].tagLine);
+	
+}
+
+suivant.addEventListener('click', slideSuivante);
+
+function slidePrecedente(){
+	items[count].classList.remove('active');
+	puces[count].classList.remove('activee');
+
+
+	if(count > 0){
+		count--;
+	}else {
+		count = nbSlide -1;
+	}
+
+	items[count].classList.add('active');
+	puces[count].classList.add('activee');
+
+	afficheText.innerHTML = slides[count].tagLine;
+
+	console.log(count);
+	console.log(slides[count].tagLine);
+
+}
+
+precedent.addEventListener('click', slidePrecedente);
+
+
+
+
+
